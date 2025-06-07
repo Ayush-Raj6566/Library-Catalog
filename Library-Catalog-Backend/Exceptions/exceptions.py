7eb8c191952e1102e3aa89b88be821f9bcbb943d
Deban Kumar Sahu <sahudebankumar@gmail.com>
2025-06-07 13:38:30 +0000

    modifying exceptions

--- Library-Catalog-Backend/Exceptions/exceptions.py
+++ Library-Catalog-Backend/Exceptions/exceptions.py
@@ -6,3 +6,9 @@ wrong_user_access = HTTPException(
         )
 
 user_not_found = HTTPException(status_code=404, detail="User not found")
+
+def item_not_found(item_id: int):
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Item with id {item_id} not found"
+    )
