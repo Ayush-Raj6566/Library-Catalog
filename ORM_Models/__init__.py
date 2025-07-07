b60c4c5f0032a8681e96162be771bee6d3dd4863
Deban Kumar Sahu <sahudebankumar@gmail.com>
2025-07-07 17:22:25 +0000

    Change in project structure

--- ORM_Models/__init__.py
+++ ORM_Models/__init__.py
@@ -2,8 +2,9 @@ from ORM_Models.book_models import *
 from ORM_Models.login_models import *
 from ORM_Models.profile_models import *
 from sqlmodel import SQLModel,create_engine
+from config import settings
 
 MYSQL_URL = "mysql+mysqlconnector://deban:Horror@172.17.208.1:3306/serverayush"
 
-engine = create_engine(url=MYSQL_URL,echo=True)
+engine = create_engine(url=settings.db_url,echo=True)
 
